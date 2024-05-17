b0e5a2aefca592f10a914ece4f8713bdc5a298b3
Bhavishya <bhavishya1102@gmail.com>
2024-05-17 17:55:55 +0530

    handle logged in user redirection

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,18 @@
-export default function AuthLayout({
+import { routes } from "@/lib/routes";
+import { fetchUserDetails } from "@/lib/serverUtils";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
+  const user = await fetchUserDetails();
+
+  if (user.error === null) {
+    if (user.data.isOnboardingComplete) redirect(routes.dashboard.overview);
+    else redirect(routes.dashboard.onboarding);
+  }
   return (
     <div className="h-full flex items-center justify-center">{children}</div>
   );
