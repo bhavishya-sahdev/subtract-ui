ac65897ce9118e3e7a309e21c3e46a96c7fd4842
Bhavishya <bhavishya1102@gmail.com>
2024-05-17 17:56:22 +0530

    handle logged in user redirection landing on dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,19 @@
 import DashboardNav from "@/components/custom/DashboardNav";
+import { fetchUserDetails } from "@/lib/serverUtils";
+import { redirect } from "next/navigation";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
+  const user = await fetchUserDetails();
+
+  if (user.error !== null) redirect("/");
+
   return (
     <>
-      <DashboardNav />
+      <DashboardNav user={user.data} />
       <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
     </>
   );
