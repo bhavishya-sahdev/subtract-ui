235a5bb06d69d3c743a665b69dd4be1f77f05f69
Bhavishya <bhavishya1102@gmail.com>
2024-05-17 19:09:59 +0530

    linting and redirection fix

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,123 +1,138 @@
-"use client";
+"use client"
 
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
 
-import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/input"
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { z } from "zod";
-import { client } from "@/lib/axiosClient";
-import { zodResolver } from "@hookform/resolvers/zod";
-import api from "@/lib/api";
-import Link from "next/link";
-import { routes } from "@/lib/routes";
-import { useToast } from "@/components/ui/use-toast";
-import { useState } from "react";
-import { Loader2 } from "lucide-react";
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { z } from "zod"
+import { client } from "@/lib/axiosClient"
+import { zodResolver } from "@hookform/resolvers/zod"
+import api from "@/lib/api"
+import Link from "next/link"
+import { routes } from "@/lib/routes"
+import { useToast } from "@/components/ui/use-toast"
+import { useState } from "react"
+import { Loader2 } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 const FormSchema = z.object({
-  email: z.string(),
-  password: z.string().min(8, "Should be atleast 8 characters"),
-});
+    email: z.string(),
+    password: z.string(),
+})
 export default function LoginForm() {
-  const { toast } = useToast();
+    const { toast } = useToast()
+    const { push } = useRouter()
+    // initialize form
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            email: "",
+        },
+    })
 
-  // initialize form
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      email: "",
-    },
-  });
+    const [inProgress, setInProgress] = useState(false)
 
-  const [inProgress, setInProgress] = useState(false);
+    async function onSubmit(data: z.infer<typeof FormSchema>) {
+        try {
+            setInProgress(true)
 
-  async function onSubmit(data: z.infer<typeof FormSchema>) {
-    try {
-      setInProgress(true);
+            const res = await client.post(api.auth.signin, data)
 
-      const res = await client.post(api.auth.signin, data);
-
-      if (res.data.error) {
-        if (typeof res.data.error != "string")
-          for (const field in FormSchema.shape) {
-            if (res.data.error[field])
-              form.setError(field as keyof typeof FormSchema.shape, {
-                message: res.data.error[field][0],
-              });
-          }
-        else
-          toast({
-            title: res.data.error,
-          });
-
-        /** if error, handle errors and return */
-        return;
-      }
-    } catch (e: any) {
-      toast({
-        variant: "destructive",
-        title: "An unknown error occured.",
-        description: "Please try again in a bit!",
-      });
-      console.error(e);
+            if (res.data.error) {
+                if (typeof res.data.error != "string")
+                    for (const field in FormSchema.shape) {
+                        if (res.data.error[field])
+                            form.setError(
+                                field as keyof typeof FormSchema.shape,
+                                {
+                                    message: res.data.error[field][0],
+                                }
+                            )
+                    }
+                else
+                    toast({
+                        title: res.data.error,
+                    })
+                setInProgress(false)
+                /** if error, handle errors and return */
+                return
+            }
+            push(routes.dashboard.overview)
+        } catch (e: any) {
+            toast({
+                variant: "destructive",
+                title: "An unknown error occured.",
+                description: "Please try again in a bit!",
+            })
+        }
+        setInProgress(false)
     }
-    setInProgress(false);
-  }
 
-  return (
-    <div className="w-full max-w-sm p-4">
-      <p className="text-3xl mb-1">Login</p>
-      <p className="mb-8">Enter your email below to login to your account.</p>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="block">
-          <div className="grid gap-2">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="m@example.com" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button type="submit" className="w-full mt-4" disabled={inProgress}>
-            {inProgress && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            Sign in
-          </Button>
-        </form>
-      </Form>
-      <div className="mx-auto mt-2 text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <Link href={routes.auth.signup} className="underline">
-          Sign up
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full max-w-sm p-4">
+            <p className="text-3xl mb-1">Login</p>
+            <p className="mb-8">
+                Enter your email below to login to your account.
+            </p>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="block">
+                    <div className="grid gap-2">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="m@example.com"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <Button
+                        type="submit"
+                        className="w-full mt-4"
+                        disabled={inProgress}
+                    >
+                        {inProgress && (
+                            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                        )}
+                        Sign in
+                    </Button>
+                </form>
+            </Form>
+            <div className="mx-auto mt-2 text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link href={routes.auth.signup} className="underline">
+                    Sign up
+                </Link>
+            </div>
+        </div>
+    )
 }
