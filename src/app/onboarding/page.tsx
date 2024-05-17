a1fc8ebd47e85e34158b20bcb4571413657b78ed
Bhavishya <bhavishya1102@gmail.com>
2024-05-17 19:10:21 +0530

    wip: add state to onboarding page

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,13 +1,17 @@
-import AddDetails from "@/components/custom/onboarding/AddDetails.3";
-import GetStarted from "@/components/custom/onboarding/GetStarted.1";
-import PickSubscriptions from "@/components/custom/onboarding/PickSubscriptions.2";
+"use client"
+import AddDetails from "@/components/custom/onboarding/AddDetails.3"
+import GetStarted from "@/components/custom/onboarding/GetStarted.1"
+import PickSubscriptions from "@/components/custom/onboarding/PickSubscriptions.2"
+import { useState } from "react"
+
+const pages = [<GetStarted />, <PickSubscriptions />, <AddDetails />]
 
 export default function Onboard() {
-  return (
-    <div className="h-full flex mx-auto justify-center flex-col gap-4 max-w-sm">
-      <GetStarted />
-      <PickSubscriptions />
-      <AddDetails />
-    </div>
-  );
+    const [activePage, setActivePage] = useState(0)
+
+    return (
+        <div className="h-full flex mx-auto justify-center -mt-10 flex-col gap-4 max-w-sm">
+            {pages[activePage]}
+        </div>
+    )
 }
