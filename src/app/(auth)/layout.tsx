3258eb40f41f22a79d451d2f7d3ca9dab362d8b1
Bhavishya <bhavishya1102@gmail.com>
2024-05-17 18:00:47 +0530

    fetch user details to show relavant pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,19 +1,21 @@
-import { routes } from "@/lib/routes";
-import { fetchUserDetails } from "@/lib/serverUtils";
-import { redirect } from "next/navigation";
+import { routes } from "@/lib/routes"
+import { fetchUserDetails } from "@/lib/utils"
+import { redirect } from "next/navigation"
 
 export default async function AuthLayout({
-  children, // will be a page or nested layout
+    children, // will be a page or nested layout
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode
 }) {
-  const user = await fetchUserDetails();
+    const user = await fetchUserDetails()
 
-  if (user.error === null) {
-    if (user.data.isOnboardingComplete) redirect(routes.dashboard.overview);
-    else redirect(routes.dashboard.onboarding);
-  }
-  return (
-    <div className="h-full flex items-center justify-center">{children}</div>
-  );
+    if (user.error === null) {
+        if (user.data.isOnboardingComplete) redirect(routes.dashboard.overview)
+        else redirect(routes.dashboard.onboarding)
+    }
+    return (
+        <div className="h-full flex items-center justify-center">
+            {children}
+        </div>
+    )
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,20 @@
-import DashboardNav from "@/components/custom/DashboardNav";
-import { fetchUserDetails } from "@/lib/serverUtils";
-import { redirect } from "next/navigation";
+import DashboardNav from "@/components/custom/DashboardNav"
+import { fetchUserDetails } from "@/lib/utils"
+import { redirect } from "next/navigation"
 
 export default async function DashboardLayout({
-  children, // will be a page or nested layout
+    children, // will be a page or nested layout
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode
 }) {
-  const user = await fetchUserDetails();
+    const user = await fetchUserDetails()
 
-  if (user.error !== null) redirect("/");
+    if (user.error !== null) redirect("/")
 
-  return (
-    <>
-      <DashboardNav user={user.data} />
-      <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
-    </>
-  );
+    return (
+        <>
+            <DashboardNav user={user.data} />
+            <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
+        </>
+    )
 }
