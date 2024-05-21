a6605ce797e078a43329896b97f54b8e2d95f874
Bhavishya <bhavishya1102@gmail.com>
2024-05-21 12:41:47 +0530

    enable dark mode and whitespace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { Toaster } from "@/components/ui/toaster"
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Subtract - Tracking your subscriptions made easy",
-  description: "Subtract the headache of manually tracking your subscriptions.",
-};
+    title: "Subtract - Tracking your subscriptions made easy",
+    description:
+        "Subtract the headache of manually tracking your subscriptions.",
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Toaster />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="dark">
+            <body className={inter.className}>
+                {children}
+                <Toaster />
+            </body>
+        </html>
+    )
 }
