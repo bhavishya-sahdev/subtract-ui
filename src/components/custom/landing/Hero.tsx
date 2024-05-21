b316979e2303a1e64aae8a46fc36da45ddddefba
Bhavishya <bhavishya1102@gmail.com>
2024-05-21 12:42:24 +0530

    initialize landing page hero section

---
+++ src/components/custom/landing/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <div className="h-[60%] p-10">
+            <div className="w-full rounded-sm h-full bg-orange-400 shadow-xl hover:shadow-orange-300/30 ease-in duration-500"></div>
+        </div>
+    )
+}
