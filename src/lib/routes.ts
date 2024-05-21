2ffc72bd4397ea8fd6fb422b882929bc343d5759
Bhavishya <bhavishya1102@gmail.com>
2024-05-21 19:09:51 +0530

    add route to landing

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,13 +1,14 @@
 export const routes = {
-  dashboard: {
-    overview: "/dashboard/overview",
-    settings: "/dashboard/settings",
-    details: "/dashboard/details",
-    onboarding: "/onboarding",
-    DEFAULT: "/dashboard",
-  },
-  auth: {
-    signup: "/signup",
-    login: "/signin",
-  },
-};
+    dashboard: {
+        overview: "/dashboard/overview",
+        settings: "/dashboard/settings",
+        details: "/dashboard/details",
+        onboarding: "/onboarding",
+        DEFAULT: "/dashboard",
+    },
+    auth: {
+        signup: "/signup",
+        login: "/signin",
+    },
+    DEFAULT: "/",
+}
