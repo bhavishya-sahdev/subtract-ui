fdb43214fac1a9cfc6b251448c11b568cfa0f3fe
Bhavishya <bhavishya1102@gmail.com>
2024-05-27 20:41:08 +0530

    hero changes

--- src/components/custom/landing/Hero.tsx
+++ src/components/custom/landing/Hero.tsx
@@ -1,7 +1,10 @@
 export default function Hero() {
     return (
         <div className="h-[60%] p-10">
-            <div className="w-full rounded-sm h-full bg-orange-400 shadow-xl hover:shadow-orange-300/30 ease-in duration-500"></div>
+            <div className="w-full rounded-sm h-full bg-orange-400 shadow-xl hover:shadow-orange-300/30 ease-in duration-500 flex">
+                <div className="w-1/2 border">Hero Container 1</div>
+                <div className="w-1/2">Hero Container 2</div>
+            </div>
         </div>
     )
 }
