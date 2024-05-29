67f815ec381ccfa74378339c3acd70270157b89c
Bhavishya <bhavishya1102@gmail.com>
2024-05-29 12:18:21 +0530

    unset user on logout

--- src/components/custom/LogoutButton.tsx
+++ src/components/custom/LogoutButton.tsx
@@ -1,10 +1,9 @@
 import { handleLogout } from "@/lib/serverUtils"
+import { useUserStore } from "@/state/context/UserContext"
 
 import React from "react"
 
-type TLogoutButtonProps<
-    T extends keyof JSX.IntrinsicElements | React.ComponentType<any>
-> = {
+type TLogoutButtonProps<T extends keyof JSX.IntrinsicElements | React.ComponentType<any>> = {
     as?: T
 } & (T extends React.ComponentType<infer P>
     ? P
@@ -12,16 +11,21 @@ type TLogoutButtonProps<
     ? JSX.IntrinsicElements[T]
     : never)
 
-const LogoutButton = <
-    T extends keyof JSX.IntrinsicElements | React.ComponentType<any>
->({
+const LogoutButton = <T extends keyof JSX.IntrinsicElements | React.ComponentType<any>>({
     as: Component = "button",
     onClick,
     children,
     ...props
 }: TLogoutButtonProps<T>) => {
+    const setUser = useUserStore((store) => store.setUser)
     return (
-        <Component {...props} onClick={() => handleLogout()}>
+        <Component
+            {...props}
+            onClick={() => {
+                setUser(null)
+                handleLogout()
+            }}
+        >
             {children ? children : "Logout"}
         </Component>
     )
