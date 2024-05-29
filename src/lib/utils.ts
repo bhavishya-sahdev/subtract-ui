cd49c01d7659af0ce07a1455b3ea3e4b6459d808
Bhavishya <bhavishya1102@gmail.com>
2024-05-29 21:58:46 +0530

    move new sub initialization to a separate function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
+import { TSubscription } from "@/state/onboarding"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { v4 as uuid } from "uuid"
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
+}
+
+export const initiateNewSubscription = (subscription?: Partial<TSubscription>): TSubscription => {
+    return {
+        currencyId: "",
+        uuid: uuid(),
+        name: "",
+        renewalAmount: 0,
+        renewalPeriodEnum: "monthly",
+        subscribedOn: new Date(),
+        ...subscription,
+    }
 }
