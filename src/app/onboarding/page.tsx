e40cf4c567ba17d2ddcc188cc8fdcfd8b4a1021b
Bhavishya <bhavishya1102@gmail.com>
2024-05-31 18:40:11 +0530

    use new form type

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -3,13 +3,13 @@
 import Parent from "@/components/custom/onboarding/pages/Parent"
 import { Form } from "@/components/ui"
 import { OnboardingStoreProvider } from "@/state/context/OnboardingContext"
-import { SubscriptionFormSchema, TSubscription } from "@/state/onboarding"
+import { SubscriptionFormSchema, TOnboardingForm } from "@/state/onboarding"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 export default function Onboard() {
-    const { watch, ...form } = useForm<{ subscriptions: TSubscription[] }>({
+    const { watch, ...form } = useForm<TOnboardingForm>({
         resolver: zodResolver(z.object({ subscriptions: SubscriptionFormSchema.array() })),
         mode: "onBlur",
         shouldUnregister: false,
