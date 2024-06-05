2285268c99fbdd02ac11ece708d348aad36d4afa
Bhavishya <bhavishya1102@gmail.com>
2024-06-05 10:54:02 +0530

    chore: Update dashboard layout with sidebar navigation

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import DashboardNav from "@/components/custom/DashboardNav"
-import { routes } from "@/lib/routes"
-import { fetchUserDetails, fetchUserSubscriptions } from "@/lib/serverUtils"
-import { redirect } from "next/navigation"
+import { Button } from "@/components/ui"
+import { fetchUserSubscriptions } from "@/lib/serverUtils"
+import { Calendar, Home, Settings } from "lucide-react"
 
 export default async function DashboardLayout({
     children, // will be a page or nested layout
@@ -11,9 +11,28 @@ export default async function DashboardLayout({
     const subscriptions = await fetchUserSubscriptions()
 
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <DashboardNav />
-            <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
-        </>
+            <div className="flex flex-1">
+                <div className="w-[20%] flex justify-between flex-col max-w-[300px] border-r border-r-zinc-800">
+                    <nav className="p-4">
+                        <Button variant="ghost" className="w-full justify-start gap-2">
+                            <Home size={18} /> Overview
+                        </Button>
+                        <Button variant="ghost" className="w-full justify-start gap-2">
+                            <Calendar size={18} /> Calender
+                        </Button>
+                        <Button variant="ghost" className="w-full justify-start gap-2">
+                            <Settings size={18} /> Settings
+                        </Button>
+                    </nav>
+                    <div>
+                        <Button>Options</Button>
+                    </div>
+                </div>
+
+                <div className="space-y-4 p-8 pt-6 flex-1">{children}</div>
+            </div>
+        </div>
     )
 }
