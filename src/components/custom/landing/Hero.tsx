394c1f1017147a4d96c636ed9c3081be67c0b583
Bhavishya <bhavishya1102@gmail.com>
2024-06-10 16:35:33 +0530

    remove hero content

--- src/components/custom/landing/Hero.tsx
+++ src/components/custom/landing/Hero.tsx
@@ -1,10 +1,3 @@
 export default function Hero() {
-    return (
-        <div className="h-[60%] p-10">
-            <div className="w-full rounded-sm h-full bg-orange-400 shadow-xl hover:shadow-orange-300/30 ease-in duration-500 flex">
-                <div className="w-1/2 border">Hero Container 1</div>
-                <div className="w-1/2">Hero Container 2</div>
-            </div>
-        </div>
-    )
+    return <></>
 }
