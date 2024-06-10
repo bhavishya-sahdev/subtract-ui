6fd3ee3a1efdfe382b8137e4e2dffc5956d2c889
Bhavishya <bhavishya1102@gmail.com>
2024-06-10 16:35:23 +0530

    remove landing nav and use a placeholder nav instead

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,7 +1,5 @@
 import LandingNav from "@/components/custom/landing/Nav"
-import { routes } from "@/lib/routes"
-import { fetchUserDetails } from "@/lib/serverUtils"
-import { redirect } from "next/navigation"
+import { Menu } from "lucide-react"
 
 export default async function LandingLayout({
     children, // will be a page or nested layout
@@ -9,8 +7,13 @@ export default async function LandingLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="h-full flex flex-col">
-            <LandingNav />
+        <div className="h-full">
+            <div className="h-[60px] flex items-center p-4 justify-between bg-[#19191C]">
+                <h2 className="text-lg font-bold">Subtract</h2>
+                <div>
+                    <Menu />
+                </div>
+            </div>
             {children}
         </div>
     )
