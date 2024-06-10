3a7e7c71104e1c0d097966584e91bcbc7373c2d1
Bhavishya <bhavishya1102@gmail.com>
2024-06-10 16:46:54 +0530

    add links to nav

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,5 +1,7 @@
-import LandingNav from "@/components/custom/landing/Nav"
+import { Button, Sheet, SheetContent, SheetTrigger } from "@/components/ui"
+import { routes } from "@/lib/routes"
 import { Menu } from "lucide-react"
+import Link from "next/link"
 
 export default async function LandingLayout({
     children, // will be a page or nested layout
@@ -9,9 +11,33 @@ export default async function LandingLayout({
     return (
         <div className="h-full">
             <div className="h-[60px] flex items-center p-4 justify-between bg-[#19191C]">
-                <h2 className="text-lg font-bold">Subtract</h2>
+                <Link href={routes.DEFAULT}>
+                    <Button variant="ghost" className="text-lg font-bold hover:bg-transparent">
+                        Subtract
+                    </Button>
+                </Link>
                 <div>
-                    <Menu />
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button size="icon" variant="ghost">
+                                <Menu />
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent>
+                            <nav className="flex flex-col py-2 space-y-2">
+                                <Link href={routes.auth.login}>
+                                    <Button variant="link" className="text-foreground justify-start">
+                                        Sign in
+                                    </Button>
+                                </Link>
+                                <Link href={routes.auth.signup}>
+                                    <Button variant="link" className="text-foreground justify-start">
+                                        Sign up
+                                    </Button>
+                                </Link>
+                            </nav>
+                        </SheetContent>
+                    </Sheet>
                 </div>
             </div>
             {children}
