993aabc77fb250aefbfbe735a71daba54f908e78
Bhavishya <bhavishya1102@gmail.com>
2024-06-13 17:15:22 +0530

    update auth bg

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,5 +7,9 @@ export default async function AuthLayout({
 }: {
     children: React.ReactNode
 }) {
-    return <div className="h-full flex items-center justify-center">{children}</div>
+    return (
+        <div className="h-full flex items-center justify-center bg-gradient-to-b from-[#19191C] to-[#24242B]">
+            {children}
+        </div>
+    )
 }
