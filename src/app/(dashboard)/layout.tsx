57fc093f3e55e0fde6ececc93012133e11a6a2a8
Bhavishya <bhavishya1102@gmail.com>
2024-06-14 12:40:41 +0530

    init an outer dashboard route group

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,4 @@
 import DashboardNav from "@/components/custom/DashboardNav"
-import { Button } from "@/components/ui"
-import { Calendar, Home, Settings } from "lucide-react"
 
 export default async function DashboardLayout({
     children, // will be a page or nested layout
@@ -8,9 +6,9 @@ export default async function DashboardLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="h-full overflow-y-auto flex flex-col bg-gradient-to-b from-[#19191C] to-[#24242B]">
+        <div className="h-full flex flex-col bg-gradient-to-b from-[#19191C] to-[#24242B]">
             <DashboardNav />
-            <main className="px-8 flex flex-col justify-between h-full ">{children}</main>
+            <main className="px-8 flex flex-col justify-between h-full  overflow-y-auto">{children}</main>
             <footer className="p-2">
                 <p className="text-center text-muted-foreground text-xs">&copy; 2024 Subtract. All Rights Reserved</p>
             </footer>
