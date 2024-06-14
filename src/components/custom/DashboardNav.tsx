bf251520873a7fe8f74f945279fed145e61578db
Bhavishya <bhavishya1102@gmail.com>
2024-06-14 12:42:31 +0530

    update outer dashboard nav

--- src/components/custom/DashboardNav.tsx
+++ src/components/custom/DashboardNav.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 import { GearIcon } from "@radix-ui/react-icons"
 import { Input } from "@/components/ui/input"
@@ -18,29 +19,42 @@ import { routes } from "@/lib/routes"
 import LogoutButton from "./LogoutButton"
 import { LogOut } from "lucide-react"
 import { useUserStore } from "@/state/context/UserContext"
+import { cn } from "@/lib/utils"
 
 const DashboardNav = () => {
     const user = useUserStore((state) => state.user)
 
+    // match path
+    const pathname = usePathname()
     if (user === null) return null
     return (
         <nav>
             <div className="flex h-16 items-center px-4">
                 <div className="flex items-center space-x-4 lg:space-x-6 mx-4">
-                    <Link href={routes.DEFAULT} className="text-sm font-medium transition-colors hover:text-primary">
-                        Home
+                    <Link href={routes.DEFAULT}>
+                        <Button
+                            variant="link"
+                            className={cn(
+                                "w-full text-muted-foreground p-0",
+                                pathname === routes.DEFAULT && "text-foreground"
+                            )}
+                        >
+                            Home
+                        </Button>
                     </Link>
-                    <Link
-                        href={routes.dashboard.overview}
-                        className="text-sm font-medium transition-colors hover:text-primary"
-                    >
-                        Dashboard
-                    </Link>
-                    <Link
-                        href={routes.dashboard.settings}
-                        className="text-sm font-medium transition-colors hover:text-primary"
-                    >
-                        Settings
+
+                    <Link href={routes.dashboard.overview}>
+                        <Button
+                            variant="link"
+                            className={cn(
+                                "w-full text-muted-foreground p-0",
+                                pathname.startsWith(routes.dashboard.DEFAULT) &&
+                                    pathname !== routes.dashboard.settings &&
+                                    "text-foreground"
+                            )}
+                        >
+                            Dashboard
+                        </Button>
                     </Link>
                 </div>
                 <div className="ml-auto flex items-center space-x-4">
