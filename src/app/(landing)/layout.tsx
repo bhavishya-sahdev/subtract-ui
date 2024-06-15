af1da14143ee211503446e4060da4ab6a5760ee7
Bhavishya <bhavishya1102@gmail.com>
2024-06-15 15:27:50 +0530

    chore: Refactor LandingLayout to include LandingNav component

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,3 +1,4 @@
+import LandingNav from "@/components/custom/landing/Nav"
 import { Button, Sheet, SheetContent, SheetTrigger } from "@/components/ui"
 import { routes } from "@/lib/routes"
 import { Menu } from "lucide-react"
@@ -9,50 +10,9 @@ export default async function LandingLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="h-full">
-            <div className="h-[60px] flex items-center p-4 justify-between bg-[#19191C]">
-                <Link href={routes.DEFAULT}>
-                    <Button variant="ghost" className="text-lg font-bold px-0 hover:bg-transparent">
-                        Subtract
-                    </Button>
-                </Link>
-                <div>
-                    <div className="hidden md:flex">
-                        <Link href={routes.auth.login}>
-                            <Button variant="link" className="text-foreground justify-start">
-                                Sign in
-                            </Button>
-                        </Link>
-                        <Link href={routes.auth.signup}>
-                            <Button variant="link" className="text-foreground justify-start">
-                                Sign up
-                            </Button>
-                        </Link>
-                    </div>
-                    <Sheet>
-                        <SheetTrigger asChild className="sm:hidden">
-                            <Button size="icon" variant="ghost">
-                                <Menu />
-                            </Button>
-                        </SheetTrigger>
-                        <SheetContent>
-                            <nav className="flex flex-col py-2 space-y-2">
-                                <Link href={routes.auth.login}>
-                                    <Button variant="link" className="text-foreground justify-start">
-                                        Sign in
-                                    </Button>
-                                </Link>
-                                <Link href={routes.auth.signup}>
-                                    <Button variant="link" className="text-foreground justify-start">
-                                        Sign up
-                                    </Button>
-                                </Link>
-                            </nav>
-                        </SheetContent>
-                    </Sheet>
-                </div>
-            </div>
-            {children}
+        <div className="h-full flex flex-col">
+            <LandingNav />
+            <div className="h-full overflow-auto">{children}</div>
         </div>
     )
 }
