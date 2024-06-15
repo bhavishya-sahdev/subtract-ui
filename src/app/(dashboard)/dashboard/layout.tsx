633fe2be45a938f7e04f1a82555bb180f1be2168
Bhavishya <bhavishya1102@gmail.com>
2024-06-15 15:25:59 +0530

    chore: Update DashboardLayout to include dynamic page title
    ```

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils"
 import { useUserStore } from "@/state/context/UserContext"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useMemo } from "react"
 
 export default function DashboardLayout({
     children, // will be a page or nested layout
@@ -15,22 +16,34 @@ export default function DashboardLayout({
     const user = useUserStore((state) => state.user)
 
     const pathname = usePathname()
+
+    const identifyPage = useMemo(() => {
+        if (pathname === routes.dashboard.subscriptions) return "Subscriptions"
+        if (pathname === routes.dashboard.payments) return "Payments"
+        return "Overview"
+    }, [pathname])
+
     if (!user) return
 
     return (
         <div>
             <div className="py-4">
                 <p className="text-3xl font-semibold">Welcome, {user.name}</p>
-                <p className="text-muted-foreground text-lg">Here&apos;s a quick overview of your account</p>
+                <p className="text-muted-foreground text-lg">
+                    {identifyPage === "Overview"
+                        ? "Here's a quick overview of your account"
+                        : identifyPage === "Payments"
+                        ? "Here are your payments"
+                        : "Here are your subscriptions"}
+                </p>
             </div>
             <nav className="flex gap-4">
                 <Link href={routes.dashboard.overview}>
                     <Button
+                        tabIndex={-1}
                         className={cn(
                             "w-full text-muted-foreground p-0",
-                            pathname.startsWith(routes.dashboard.DEFAULT) &&
-                                pathname !== routes.dashboard.subscriptions &&
-                                "text-foreground"
+                            identifyPage === "Overview" && "text-foreground"
                         )}
                         variant="link"
                     >
@@ -39,18 +52,23 @@ export default function DashboardLayout({
                 </Link>
                 <Link href={routes.dashboard.subscriptions}>
                     <Button
+                        tabIndex={-1}
                         className={cn(
                             "w-full text-muted-foreground p-0",
-                            pathname === routes.dashboard.subscriptions && "text-foreground"
+                            identifyPage === "Subscriptions" && "text-foreground"
                         )}
                         variant="link"
                     >
                         Subscriptions
                     </Button>
                 </Link>
-                <Link href="/">
+                <Link href={routes.dashboard.payments}>
                     <Button
-                        className={cn("w-full text-muted-foreground p-0", pathname === "TODO" && "text-foreground")}
+                        tabIndex={-1}
+                        className={cn(
+                            "w-full text-muted-foreground p-0",
+                            identifyPage === "Payments" && "text-foreground"
+                        )}
                         variant="link"
                     >
                         Payments
