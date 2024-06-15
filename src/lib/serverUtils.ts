f7cd0867952238a3030c6dcd0d11dcd2caad2f79
Bhavishya <bhavishya1102@gmail.com>
2024-06-15 15:29:57 +0530

    chore: Refactor serverUtils.ts to improve code organization and readability

--- src/lib/serverUtils.ts
+++ src/lib/serverUtils.ts
@@ -1,82 +1,109 @@
 "use server"
 
-import { TCurrency, TPrefab } from "@/state/onboarding"
+import { TPrefab } from "@/state/onboarding"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 import { cache } from "react"
 import api from "./api"
 import { client } from "./axiosClient"
-import { TAxiosErrorResponse, TAxiosSuccessResponse, TAxiosUserDetails } from "./types"
+import {
+    TAxiosCurrencyDetails,
+    TAxiosErrorResponse,
+    TAxiosPaymentDetails,
+    TAxiosSuccessResponse,
+    TAxiosUserDetails,
+} from "./types"
 
 export const fetchUserDetails = cache(
-  async (): Promise<TAxiosSuccessResponse<TAxiosUserDetails> | TAxiosErrorResponse<{ message: string }>> => {
-    const cookieStore = cookies()
-    const token = cookieStore.get("token")
-    try {
-      const res = await client.get(api.user.getSubscriptions, {
-        headers: {
-          Authorization: `Bearer ${token ? token.value : ""}`,
-        },
-      })
-      return res.data
-    } catch (error) {
-      return {
-        data: null,
-        error: { message: "Failed to load user data" },
-      }
+    async (): Promise<TAxiosSuccessResponse<TAxiosUserDetails> | TAxiosErrorResponse<{ message: string }>> => {
+        const cookieStore = cookies()
+        const token = cookieStore.get("token")
+        try {
+            const res = await client.get(api.user.getSubscriptions, {
+                headers: {
+                    Authorization: `Bearer ${token ? token.value : ""}`,
+                },
+            })
+            return res.data
+        } catch (error) {
+            return {
+                data: null,
+                error: { message: "Failed to load user data" },
+            }
+        }
     }
-  }
 )
 
 export const fetchAllCurrencies = cache(
-  async (): Promise<TAxiosSuccessResponse<TCurrency[]> | TAxiosErrorResponse<{ message: string }>> => {
-    try {
-      const res = await client.get(api.utils.currency)
-      return res.data
-    } catch (error) {
-      return {
-        data: null,
-        error: { message: "Failed to load currency list" },
-      }
+    async (): Promise<TAxiosSuccessResponse<TAxiosCurrencyDetails[]> | TAxiosErrorResponse<{ message: string }>> => {
+        try {
+            const res = await client.get(api.utils.currency)
+            return res.data
+        } catch (error) {
+            return {
+                data: null,
+                error: { message: "Failed to load currency list" },
+            }
+        }
     }
-  }
 )
 
 export const fetchUserSubscriptions = cache(async () => {
-  const cookieStore = cookies()
-  const token = cookieStore.get("token")
-  try {
-    const res = await client.get(api.user.getSubscriptions, {
-      headers: {
-        Authorization: `Bearer ${token ? token.value : ""}`,
-      },
-    })
+    const cookieStore = cookies()
+    const token = cookieStore.get("token")
+    try {
+        const res = await client.get(api.user.getSubscriptions, {
+            headers: {
+                Authorization: `Bearer ${token ? token.value : ""}`,
+            },
+        })
 
-    return res.data
-  } catch (error) {
-    return {
-      data: null,
-      error: { message: "Failed to load subscription data" },
+        return res.data
+    } catch (error) {
+        return {
+            data: null,
+            error: { message: "Failed to load subscription data" },
+        }
     }
-  }
 })
 
-export const fetchPrefabs = cache(
-  async (): Promise<TAxiosSuccessResponse<TPrefab[]> | TAxiosErrorResponse<{ message: string }>> => {
-    try {
-      const res = await client.get(api.utils.prefab)
-      return res.data
-    } catch (error) {
-      return {
-        data: null,
-        error: { message: "Failed to load prefab list" },
-      }
+export const fetchUserPayments = cache(
+    async (): Promise<TAxiosSuccessResponse<TAxiosPaymentDetails[]> | TAxiosErrorResponse<{ message: string }>> => {
+        const cookieStore = cookies()
+        const token = cookieStore.get("token")
+        try {
+            const res = await client.get(api.user.getPayments, {
+                headers: {
+                    Authorization: `Bearer ${token ? token.value : ""}`,
+                },
+            })
+
+            return res.data
+        } catch (error) {
+            return {
+                data: null,
+                error: { message: "Failed to load payment data" },
+            }
+        }
     }
-  }
+)
+
+export const fetchPrefabs = cache(
+    async (): Promise<TAxiosSuccessResponse<TPrefab[]> | TAxiosErrorResponse<{ message: string }>> => {
+        try {
+            const res = await client.get(api.utils.prefab)
+            return res.data
+        } catch (error) {
+            return {
+                data: null,
+                error: { message: "Failed to load prefab list" },
+            }
+        }
+    }
 )
 
 export async function handleLogout() {
-  const cookieStore = cookies()
-  cookieStore.delete("token")
-  redirect("/")
+    const cookieStore = cookies()
+    cookieStore.delete("token")
+    redirect("/")
 }
