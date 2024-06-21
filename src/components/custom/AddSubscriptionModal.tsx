0fb61820de15f40a26665db9aa006ce711bfe0ac
Bhavishya <bhavishya1102@gmail.com>
2024-06-21 18:59:42 +0530

    chore: add addsubscriptionmodal

--- src/components/custom/AddSubscriptionModal.tsx
+++ src/components/custom/AddSubscriptionModal.tsx
@@ -1,211 +1,145 @@
-"use client";
+"use client"
 
-import { Input } from "@/components/ui/input";
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Label } from "@/components/ui/label";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { CalendarIcon, PlusIcon } from "@radix-ui/react-icons";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { useState } from "react";
-import { format } from "date-fns";
-import { cn } from "@/lib/utils";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import { useState, ReactNode, useEffect } from "react"
+import { addDays } from "date-fns"
+import { createSubscriptionsWithPayments, generatePayments, updateUserOnboardingStatus } from "@/lib/utils"
 
-export default function AddSubscriptionModal() {
-  const [date, setDate] = useState<Date | undefined>();
+import AddSubscriptionForm from "./AddSubscriptionForm"
+import { Form, useToast } from "../ui"
+import { SubscriptionFormSchema, TPayment, TSubscription } from "@/state/onboarding"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useUserStore } from "@/state/context/UserContext"
+import { fetchUserDetails, fetchUserPayments } from "@/lib/serverUtils"
+import { TServerError } from "@/lib/types"
 
-  return (
-    <>
-      <Dialog modal>
-        <DialogTrigger asChild>
-          <Button>
-            <PlusIcon className="mr-2 h-4 w-4" />
-            Add manually
-          </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader className="mb-4">
-            <DialogTitle>Add Subscription</DialogTitle>
-          </DialogHeader>
+type TAddSubscriptionModalProps = {
+    children: ReactNode
+}
 
-          <form>
-            <div className="space-y-4">
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="service_name">Service Name</Label>
-                <Input id="service_name" type="text" placeholder="Netflix" />
-              </div>
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="service_url">Service Website</Label>
-                <Input
-                  id="service_url"
-                  type="url"
-                  placeholder="https://netflix.com"
-                />
-              </div>
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="subscription_created_date">
-                  Subscription Created On
-                </Label>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      id="subscription_created_date"
-                      variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !date && "text-muted-foreground"
-                      )}
-                    >
-                      {date ? format(date, "PPP") : <span>Pick a date</span>}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={date}
-                      onSelect={setDate}
-                      disabled={(date) =>
-                        date > new Date() || date < new Date("1900-01-01")
-                      }
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-              </div>
+const formDefaultValues: Omit<TSubscription, "uuid"> = {
+    name: "",
+    upcomingPaymentDate: addDays(new Date(), 30),
+    creationDate: new Date(),
+    renewalAmount: 0,
+    currencyId: "",
+    renewalPeriodEnum: "monthly",
+    renewalPeriodDays: 1,
+}
 
-              {/* add day counter for renewal period, show next renewal on `date` */}
+export default function AddSubscriptionModal({ children }: TAddSubscriptionModalProps) {
+    const { setUser, setPayments, setSubscriptions } = useUserStore((state) => state)
+    const { toast } = useToast()
 
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="subscription_charges">
-                  Subscription Charges
-                </Label>
-                <div className="flex gap-2" id="subscription_charges">
-                  <Select>
-                    <SelectTrigger className="w-[280px]">
-                      <SelectValue placeholder="Select a currency" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>North America</SelectLabel>
-                        <SelectItem value="est">
-                          Eastern Standard Time (EST)
-                        </SelectItem>
-                        <SelectItem value="cst">
-                          Central Standard Time (CST)
-                        </SelectItem>
-                        <SelectItem value="mst">
-                          Mountain Standard Time (MST)
-                        </SelectItem>
-                        <SelectItem value="pst">
-                          Pacific Standard Time (PST)
-                        </SelectItem>
-                        <SelectItem value="akst">
-                          Alaska Standard Time (AKST)
-                        </SelectItem>
-                        <SelectItem value="hst">
-                          Hawaii Standard Time (HST)
-                        </SelectItem>
-                      </SelectGroup>
-                      <SelectGroup>
-                        <SelectLabel>Europe & Africa</SelectLabel>
-                        <SelectItem value="gmt">
-                          Greenwich Mean Time (GMT)
-                        </SelectItem>
-                        <SelectItem value="cet">
-                          Central European Time (CET)
-                        </SelectItem>
-                        <SelectItem value="eet">
-                          Eastern European Time (EET)
-                        </SelectItem>
-                        <SelectItem value="west">
-                          Western European Summer Time (WEST)
-                        </SelectItem>
-                        <SelectItem value="cat">
-                          Central Africa Time (CAT)
-                        </SelectItem>
-                        <SelectItem value="eat">
-                          East Africa Time (EAT)
-                        </SelectItem>
-                      </SelectGroup>
-                      <SelectGroup>
-                        <SelectLabel>Asia</SelectLabel>
-                        <SelectItem value="msk">Moscow Time (MSK)</SelectItem>
-                        <SelectItem value="ist">
-                          India Standard Time (IST)
-                        </SelectItem>
-                        <SelectItem value="cst_china">
-                          China Standard Time (CST)
-                        </SelectItem>
-                        <SelectItem value="jst">
-                          Japan Standard Time (JST)
-                        </SelectItem>
-                        <SelectItem value="kst">
-                          Korea Standard Time (KST)
-                        </SelectItem>
-                        <SelectItem value="ist_indonesia">
-                          Indonesia Central Standard Time (WITA)
-                        </SelectItem>
-                      </SelectGroup>
-                      <SelectGroup>
-                        <SelectLabel>Australia & Pacific</SelectLabel>
-                        <SelectItem value="awst">
-                          Australian Western Standard Time (AWST)
-                        </SelectItem>
-                        <SelectItem value="acst">
-                          Australian Central Standard Time (ACST)
-                        </SelectItem>
-                        <SelectItem value="aest">
-                          Australian Eastern Standard Time (AEST)
-                        </SelectItem>
-                        <SelectItem value="nzst">
-                          New Zealand Standard Time (NZST)
-                        </SelectItem>
-                        <SelectItem value="fjt">Fiji Time (FJT)</SelectItem>
-                      </SelectGroup>
-                      <SelectGroup>
-                        <SelectLabel>South America</SelectLabel>
-                        <SelectItem value="art">
-                          Argentina Time (ART)
-                        </SelectItem>
-                        <SelectItem value="bot">Bolivia Time (BOT)</SelectItem>
-                        <SelectItem value="brt">Brasilia Time (BRT)</SelectItem>
-                        <SelectItem value="clt">
-                          Chile Standard Time (CLT)
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                  <Input type="number" placeholder="12.99" />
+    const [inProgress, setInProgress] = useState(false)
+    const [openDialog, setOpenDialog] = useState(false)
+
+    const form = useForm<Omit<TSubscription, "uuid">>({
+        resolver: zodResolver(SubscriptionFormSchema.omit({ uuid: true })),
+        defaultValues: formDefaultValues,
+        mode: "onBlur",
+        shouldUnregister: false,
+    })
+    const [formPayments, setFormPayments] = useState<TPayment[]>([])
+
+    const watchRenewalPeriodEnum = form.watch(`renewalPeriodEnum`)
+    const watchRenewalPeriodDays = form.watch(`renewalPeriodDays`)
+    const watchCreationDate = form.watch(`creationDate`)
+    const watchCurrencyId = form.watch(`currencyId`)
+    const watchRenewalAmount = form.watch(`renewalAmount`)
+
+    useEffect(() => {
+        if (watchCreationDate && watchRenewalPeriodEnum) {
+            const createdPayments = generatePayments({
+                creationDate: watchCreationDate,
+                renewalPeriodEnum: watchRenewalPeriodEnum,
+                amount: watchRenewalAmount,
+                currencyId: watchCurrencyId,
+                renewalPeriodDays: watchRenewalPeriodDays,
+            })
+            setFormPayments(createdPayments)
+            form.setValue(`upcomingPaymentDate`, createdPayments[0].date)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [watchRenewalPeriodDays, watchRenewalPeriodEnum, watchCreationDate, watchRenewalAmount, watchCurrencyId])
+
+    useEffect(() => {
+        console.log(form.formState.errors)
+    }, [form.formState.errors])
+
+    async function onSubmit(data: Omit<TSubscription, "uuid">) {
+        try {
+            setInProgress(true)
+            const res = await createSubscriptionsWithPayments({
+                subscriptions: [data],
+                payments: [formPayments],
+            })
+
+            if (res.data !== null) {
+                const onboardingStatusRes = await updateUserOnboardingStatus()
+                const updatedUser = await fetchUserDetails()
+                const payments = await fetchUserPayments()
+                if (updatedUser.data === null || payments.data === null)
+                    return toast({
+                        title: "An error occured while fetching user details. Please try refreshing the page",
+                    })
+
+                toast({ title: "Subscription added" })
+                if (onboardingStatusRes.data !== null) {
+                    setUser(updatedUser.data)
+                    setSubscriptions(updatedUser.data.subscriptions)
+                    setPayments(payments.data)
+                    form.reset(formDefaultValues)
+                    setOpenDialog(false)
+                }
+            } else {
+                // handle backend zod errors on fields
+                const zodErrors = res.error
+                if ("message" in res.error) {
+                    toast({
+                        variant: "destructive",
+                        title: (zodErrors as TServerError).message,
+                    })
+                } else {
+                    // (zodErrors as z.ZodIssue[]).forEach((error) => {
+                    //     setError(error.path[0], { message: error.message })
+                    // })
+                    // console.log(res.data)
+                }
+            }
+        } catch (e: any) {
+            toast({
+                variant: "destructive",
+                title: "An unknown error occured.",
+                description: "Please try again in a bit!",
+            })
+        }
+        setInProgress(false)
+    }
+
+    return (
+        <Dialog modal open={openDialog} onOpenChange={() => setOpenDialog(!openDialog)}>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+            <DialogContent className="max-h-screen overflow-y-auto overflow-x-hidden">
+                <DialogHeader className="mb-4">
+                    <DialogTitle>Add Subscription</DialogTitle>
+                </DialogHeader>
+                <div className="w-full">
+                    <Form {...form}>
+                        <form>
+                            <AddSubscriptionForm />
+                        </form>
+                    </Form>
                 </div>
-              </div>
-            </div>
-          </form>
-          <DialogFooter className="sm:justify-start">
-            <Button>Add</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
-  );
+                <DialogFooter className="sm:justify-start">
+                    <Button onClick={form.handleSubmit(onSubmit)}>Add Subscription</Button>
+                    <Button variant="secondary" onClick={() => setOpenDialog(false)}>
+                        Cancel
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
 }
