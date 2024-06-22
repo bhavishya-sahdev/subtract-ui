9e9c72265c568232b68f04c54bbc1e0dcf02693e
Bhavishya <bhavishya1102@gmail.com>
2024-06-22 17:50:30 +0530

    chore: Update serverUtils.ts and utils.ts

--- src/lib/serverUtils.ts
+++ src/lib/serverUtils.ts
@@ -14,6 +14,42 @@ import {
     TAxiosUserDetails,
 } from "./types"
 
+export const authWithGoogle = async (
+    code: string
+): Promise<TAxiosSuccessResponse<{ token: string }> | TAxiosErrorResponse<{ message: string }>> => {
+    try {
+        const res = await client.post(api.auth.google, {
+            code,
+        })
+        return res.data
+    } catch (error) {
+        return {
+            data: null,
+            error: { message: "Failed to authenticate with Google" },
+        }
+    }
+}
+
+export const fetchUserDetails = cache(
+    async (): Promise<TAxiosSuccessResponse<TAxiosUserDetails> | TAxiosErrorResponse<{ message: string }>> => {
+        const cookieStore = cookies()
+        const token = cookieStore.get("token")
+        try {
+            const res = await client.get(api.user.getSubscriptions, {
+                headers: {
+                    Cookie: `token=${token ? token.value : ""}`,
+                },
+            })
+            return res.data
+        } catch (error) {
+            return {
+                data: null,
+                error: { message: "Failed to load user data" },
+            }
+        }
+    }
+)
+
 export const fetchAllCurrencies = cache(
     async (): Promise<TAxiosSuccessResponse<TAxiosCurrencyDetails[]> | TAxiosErrorResponse<{ message: string }>> => {
         try {
@@ -23,6 +59,46 @@ export const fetchAllCurrencies = cache(
             return {
                 data: null,
                 error: { message: "Failed to load currency list" },
+            }
+        }
+    }
+)
+
+export const fetchUserSubscriptions = cache(async () => {
+    const cookieStore = cookies()
+    const token = cookieStore.get("token")
+    try {
+        const res = await client.get(api.user.getSubscriptions, {
+            headers: {
+                Authorization: `Bearer ${token ? token.value : ""}`,
+            },
+        })
+
+        return res.data
+    } catch (error) {
+        return {
+            data: null,
+            error: { message: "Failed to load subscription data" },
+        }
+    }
+})
+
+export const fetchUserPayments = cache(
+    async (): Promise<TAxiosSuccessResponse<TAxiosPaymentDetails[]> | TAxiosErrorResponse<{ message: string }>> => {
+        const cookieStore = cookies()
+        const token = cookieStore.get("token")
+        try {
+            const res = await client.get(api.user.getPayments, {
+                headers: {
+                    Cookie: `token=${token ? token.value : ""}`,
+                },
+            })
+
+            return res.data
+        } catch (error) {
+            return {
+                data: null,
+                error: { message: "Failed to load payment data" },
             }
         }
     }
