8bf97e333a342da05c64898ada2d9524ab923871
Bhavishya <bhavishya1102@gmail.com>
2024-06-24 10:52:21 +0530

    chore: Refactor payments page to use updated components and add search functionality

--- src/app/(dashboard)/dashboard/payments/page.tsx
+++ src/app/(dashboard)/dashboard/payments/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 
-import PaymentsTable from "@/components/custom/dashboard/PaymentsTable"
-import { useUserStore } from "@/state/context/UserContext"
+import PaymentsTable from "@/components/custom/dashboard/PaymentsTableForPaymentsPage"
+import { Button, Input } from "@/components/ui"
 
 export default function Payments() {
-    const payments = useUserStore((state) => state.payments)
-
     return (
-        <div className="py-4">
-            <PaymentsTable payments={payments} />
+        <div className="py-4 space-y-2">
+            <div className="flex justify-end w-1/2">
+                <Button variant="outline">Add New</Button>
+                <Input placeholder="Search By Subscription" />
+            </div>
+            <PaymentsTable />
         </div>
     )
 }
