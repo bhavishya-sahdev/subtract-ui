b419ee01d254ff70930171c93b8b7a9faaba3d01
Bhavishya <bhavishya1102@gmail.com>
2024-07-03 18:56:05 +0530

    feat: styling for payments page

--- src/app/(dashboard)/dashboard/payments/page.tsx
+++ src/app/(dashboard)/dashboard/payments/page.tsx
@@ -5,10 +5,9 @@ import { Button, Input } from "@/components/ui"
 
 export default function Payments() {
     return (
-        <div className="py-4 space-y-2">
-            <div className="flex justify-end w-1/2">
+        <div className="py-4 space-y-4">
+            <div className="flex gap-1 justify-end w-1/2">
                 <Button variant="outline">Add New</Button>
-                <Input placeholder="Search By Subscription" />
             </div>
             <PaymentsTable />
         </div>
