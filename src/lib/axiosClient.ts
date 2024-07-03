b8ce157f861a689420289d1b222c01b957491c7f
Bhavishya <bhavishya1102@gmail.com>
2024-07-03 18:59:07 +0530

    refactor: Update axiosClient.ts to use single quotes for consistency
    Update the axiosClient.ts file to use single quotes for string literals instead of double quotes. This change ensures consistency in the codebase and follows the established convention of using single quotes for string literals. It improves code readability and maintainability.

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -1,22 +1,22 @@
-import { Axios } from "axios";
+import { Axios } from "axios"
 
 export const client = new Axios({
-  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  /** Needed if you want to set cookies on the frontend */
-  withCredentials: true,
-  transformRequest: [
-    function (data) {
-      // Automatically parse data as JSON
-      return JSON.stringify(data);
+    baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    headers: {
+        "Content-Type": "application/json",
     },
-  ],
-  transformResponse: [
-    function (data) {
-      // Automatically parse data as JSON
-      return JSON.parse(data);
-    },
-  ],
-});
+    /** Needed if you want to set cookies on the frontend */
+    withCredentials: true,
+    transformRequest: [
+        function (data) {
+            // Automatically parse data as JSON
+            return JSON.stringify(data)
+        },
+    ],
+    transformResponse: [
+        function (data) {
+            // Automatically parse data as JSON
+            return JSON.parse(data)
+        },
+    ],
+})
